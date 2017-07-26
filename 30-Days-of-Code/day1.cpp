8361446a580c86949f0f9354d9975eff44b79e0b
Joshua Johnson <joshuaj1397@gmail.com>
2017-07-25 22:06:33 -0500

    Fixed floating point precision bug in 30-Days-of-Code/day1

--- 30-Days-of-Code/day1.cpp
+++ 30-Days-of-Code/day1.cpp
@@ -5,20 +5,20 @@
 using namespace std;
 
 int main() {
-    int myInt;
-    cin >> myInt;
-    double myDouble;
-    string myString;
     int i = 4;
     double d = 4.0;
     string s = "HackerRank ";
-
+    int myInt;
+    double myDouble;
+    string myString;
     cin >> myInt;
     cin >> myDouble;
+    cin.ignore();
     getline(cin, myString);
 
     cout << myInt + i << endl;
-    cout << setprecision(1) << myDouble + d << endl;
+    cout << fixed << setprecision(1) << myDouble + d << endl;
     cout << s << myString << endl;
+
     return 0;
 }
