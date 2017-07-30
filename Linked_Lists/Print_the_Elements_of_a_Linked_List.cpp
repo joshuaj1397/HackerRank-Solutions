aa26468637b17dbd19c80439361e15b3220bfc88
Joshua Johnson <joshuaj1397@gmail.com>
2017-07-29 23:05:21 -0500

    Added solution for "Print Elements of a Linked List" in Linked Lists

---
+++ Linked_Lists/Print_the_Elements_of_a_Linked_List.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+/*
+  Print elements of a linked list on console
+  head pointer input could be NULL as well for empty list
+  Node is defined as
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+void Print(Node *head)
+{
+    if (head == NULL) {
+        // Do nothing
+    }
+    else {
+        cout << head->data << endl;
+        Print(head->next);
+    }
+}
